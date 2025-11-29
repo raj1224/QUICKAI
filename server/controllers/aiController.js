e6f8c7db87f80b6dfafdb259bf74d7c42fe7cab1
Raj Kumar <imraaj1026@gmail.com>
2025-11-29 10:16:04 +0530

    background remove done

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -162,7 +162,7 @@ res.json({success: true, content});
 export const removeImageBackground= async(req ,res)=>{
     try {
         const {userId}= req.auth();
-        const {image}=req.file;
+        const image=req.file;
         const plan= req.plan;
 
         if(plan!=='premium'  ){
